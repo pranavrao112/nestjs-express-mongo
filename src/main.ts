d5b55ac8fce7f87e18413fefe97f24fa8008b1de
Rao <Pranav_Rao@McAfee.com>
2022-05-31 01:49:51 +0530

    added logic to handle process var

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,9 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(process.env.PORT || 3000);
+
+  const port: number = parseInt(`${process.env.PORT}`) || 3000;
+  await app.listen(port);
 }
 bootstrap();
 
